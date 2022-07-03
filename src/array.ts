ddfc5570f6099b3da23c5faeee490c39adcebe8a
gtoootg0131 <gotosan0131@yahoo.co.jp>
2022-07-03 15:10:51 +0200

    update

---
+++ src/array.ts
@@ -0,0 +1,17 @@
+export {};
+
+//数字
+let numbers: number[] = [1, 2, 3];
+//非推奨
+// let number2: Array<number> = [1,2,3];
+
+//文字列
+let strings: string[] = ["TypeScript", "JavaScript", "CoffeScript"];
+
+//2次元配列
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, "Japan"];
---
+++ src/enum.ts
@@ -0,0 +1,31 @@
+enum Months {
+  January = 1,
+  Feburary = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12,
+}
+
+enum COLORS {
+  RED = "#FF0000",
+  WHITE = "#FFFFFF",
+  GREEN = "#008000",
+  BLUE = "#0000FF",
+  BLACK = "#000000",
+}
+
+let green = COLORS.GREEN;
+console.log({ green });
+
+enum COLORS {
+  YELLOW = "#FFFF00",
+}
+
+console.log(COLORS);
